6a5f48a91e32f458777a95da940810d9ff5ec161
andikaneviantoro <neviantoroa@gmail.com>
2022-10-25 10:46:32 +0700

    third commit

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HeloController;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,4 @@ Route::get('halo',function(){
 Route::resource('helo', HeloController::class);
 
 Route::resource('siswa', SiswaController::class);
+Route::resource('books', BookController::class);
